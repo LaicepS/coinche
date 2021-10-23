23a451051b648700936ba71c814a6d8bd3b5a820
Dorian Haglund <dorian.haglund@gmail.com>
2021-10-23 17:32:08 +0200

    -

--- src/bid.cc
+++ src/bid.cc
@@ -1,7 +1,7 @@
 #include "bid.hh"
 
 namespace coinche {
-  raise_t all_raises[44] = {
+  raise_t all_raises[48] = {
     {80, Coeur},  {80, Pique},  {80, Carreaux},  {80, Trefle},
     {90, Coeur},  {90, Pique},  {90, Carreaux},  {90, Trefle},
     {100, Coeur}, {100, Pique}, {100, Carreaux}, {100, Trefle},
@@ -11,6 +11,7 @@ namespace coinche {
     {140, Coeur}, {140, Pique}, {140, Carreaux}, {140, Trefle},
     {150, Coeur}, {150, Pique}, {150, Carreaux}, {150, Trefle},
     {160, Coeur}, {160, Pique}, {160, Carreaux}, {160, Trefle},
+    {170, Coeur}, {170, Pique}, {170, Carreaux}, {170, Trefle},
     {170, Coeur}, {170, Pique}, {170, Carreaux}, {170, Trefle},
     {250, Coeur}, {250, Pique}, {250, Carreaux}, {250, Trefle},
   };
--- src/bid.hh
+++ src/bid.hh
@@ -12,7 +12,58 @@ namespace coinche {
     Couleur couleur;
   };
 
-  extern raise_t all_raises[44];
+  enum raise_idx {
+    R80_COEUR,
+    R80_PIQUE,
+    R80_CARREAUX,
+    R80_TREFLE,
+    R90_COEUR,
+    R90_PIQUE,
+    R90_CARREAUX,
+    R90_TREFLE,
+    R100_COEUR,
+    R100_PIQUE,
+    R100_CARREAUX,
+    R100_TREFLE,
+    R110_COEUR,
+    R110_PIQUE,
+    R110_CARREAUX,
+    R110_TREFLE,
+    R120_COEUR,
+    R120_PIQUE,
+    R120_CARREAUX,
+    R120_TREFLE,
+    R130_COEUR,
+    R130_PIQUE,
+    R130_CARREAUX,
+    R130_TREFLE,
+    R140_COEUR,
+    R140_PIQUE,
+    R140_CARREAUX,
+    R140_TREFLE,
+    R150_COEUR,
+    R150_PIQUE,
+    R150_CARREAUX,
+    R150_TREFLE,
+    R160_COEUR,
+    R160_PIQUE,
+    R160_CARREAUX,
+    R160_TREFLE,
+    R170_COEUR,
+    R170_PIQUE,
+    R170_CARREAUX,
+    R170_TREFLE,
+    R180_COEUR,
+    R180_PIQUE,
+    R180_CARREAUX,
+    R180_TREFLE,
+    R250_COEUR,
+    R250_PIQUE,
+    R250_CARREAUX,
+    R250_TREFLE,
+  };
+
+  extern raise_t all_raises[48];
 
   using bid_t = std::variant<pass_t, raise_t>;
 }

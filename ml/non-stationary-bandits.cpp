c929c026651fbda2f8558dca86de183f2abac768
Dorian Haglund <dorian.haglund@gmail.com>
2021-10-25 20:06:18 +0200

    -

---
+++ ml/non-stationary-bandits.cpp
@@ -0,0 +1,57 @@
+#include <cassert>
+#include <chrono>
+#include <iostream>
+#include <random>
+
+struct bandit_estimation_t {
+  double current_estimation = 0;
+  int selected_count = 0;
+};
+
+using normal_distribution_t = std::normal_distribution<double>;
+
+double epsilon = 0.1;
+
+int main() {
+  unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+  std::default_random_engine generator(seed);
+
+  // std::vector<std::normal_distribution<double>> bandits(
+  //     10, std::normal_distribution<double>{0, 1});
+
+  std::vector<normal_distribution_t> bandits;
+  std::vector<bandit_estimation_t> bandit_estimations;
+  for (int i = -5; i < 5; i++) {
+    bandits.emplace_back(i, 1);
+    bandit_estimations.emplace({});
+  }
+
+  std::uniform_real_distribution<double> dice(0., 1.);
+  for (int i = 0; i < 10'000; i++) {
+    auto choose_best_bandit = dice(generator) > epsilon;
+    int bandit_idx = -1;
+    if (choose_best_bandit) {
+      std::vector<int> best_bandits_idx;
+      double best_bandit_value = -1000;
+      for (int i = 0; i < bandits.size(); i++) {
+        if (bandit_estimations[i].current_estimation > best_bandit_value) {
+          best_bandit_value = bandit_estimations[i].current_estimation;
+          best_bandits_idx = std::vector<int>{i};
+        } else if (bandit_estimations[i].current_estimation ==
+                   best_bandit_value)
+          best_bandits_idx.push_back(i);
+
+        if (best_bandits_idx.size() == 1)
+          bandit_idx = best_bandits_idx.front();
+        else {
+          assert(best_bandits_idx.size() > 1);
+          std::uniform_int_distribution<int> referee(0,
+                                                     best_bandits_idx.size());
+        }
+      }
+    } else {
+    }
+  }
+
+  return 0;
+}
